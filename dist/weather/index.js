eb7cbed72c0d669d9cedbc2610d749b17a7439a3
olit kenea <olit2950@gmail.com>
2026-02-05 21:13:09 +0300

    refactor extract Weather and Forecast types integrate Zod runtime validation

--- dist/weather/index.js
+++ dist/weather/index.js
@@ -1,48 +1,23 @@
 import dotenv from "dotenv";
-import { z } from "zod";
+import { WeatherResponseSchema, ForecastResponseSchema, } from "./weatherForcast .js";
 dotenv.config();
-const city = "London";
 const APIKey = process.env.APIKEY;
-const WeatherSchema = z.object({
-    coord: z.object({ lon: z.number(), lat: z.number() }),
-    weather: z.array(z.object({
-        id: z.number(),
-        main: z.string(),
-        description: z.string(),
-        icon: z.string(),
-    })),
-    main: z.object({
-        temp: z.number(),
-        feels_like: z.number(),
-        temp_min: z.number(),
-        temp_max: z.number(),
-        pressure: z.number(),
-        humidity: z.number(),
-    }),
-    wind: z.object({ speed: z.number(), deg: z.number() }),
-    clouds: z.object({ all: z.number() }),
-    name: z.string(),
-    cod: z.number(),
-});
-const getCityWeather = async (city) => {
+async function getCityWeather(city) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=metric`;
-    try {
-        const response = await fetch(url);
-        const rawData = await response.json();
-        const provedData = WeatherSchema.parse(rawData);
-        console.log(`
-       City: ${provedData.name}
-      Condition: ${provedData?.weather[0]?.main} (${provedData?.weather[0]?.description})
-       Temperature: ${provedData.main.temp}°C (feels like ${provedData.main.feels_like}°C)
-       Min: ${provedData.main.temp_min}°C | 🔼 Max: ${provedData.main.temp_max}°C
-       Humidity: ${provedData.main.humidity}%
-       Wind: ${provedData.wind.speed} m/s from ${provedData.wind.deg}°
-       Cloud Cover: ${provedData.clouds.all}%
-      `);
-    }
-    catch (err) {
-        console.log("oh no, something went wrong", err);
-    }
-};
-getCityWeather(city);
+    const response = await fetch(url);
+    const rawData = await response.json();
+    const provedData = WeatherResponseSchema.parse(rawData);
+    console.log(`City: ${provedData.name}, Temp: ${provedData.main.temp}°C`);
+}
+async function getForecast(city) {
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=metric`;
+    const response = await fetch(url);
+    const rawData = await response.json();
+    const forecast = ForecastResponseSchema.parse(rawData);
+    forecast.list.slice(0, 3).forEach(f => {
+        console.log(`${f.dt_txt} → ${f?.weather[0]?.description}, ${f.main.temp}°C`);
+    });
+}
+getCityWeather("london");
+getForecast("london");
 //# sourceMappingURL=index.js.map
