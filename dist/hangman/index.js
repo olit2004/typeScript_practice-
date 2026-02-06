6a517a98abe932dcef58d2c9f85855b3960447d3
olit kenea <olit2950@gmail.com>
2026-02-06 21:16:22 +0300

    modularized hangman

--- dist/hangman/index.js
+++ dist/hangman/index.js
@@ -1,54 +1,11 @@
-const HANGMANPICS = [
-    `  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========`,
-    ` +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========`,
-    `  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========`,
-    `  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========`,
-    `  +---+
-  |   |
-  O   |
- /|\\  |
-      |
-      |
-=========`,
-    `  +---+
-  |   |
-  O   |
- /|\\  |
- /    |
-      |
-=========`,
-    `  +---+
-  |   |
-  O   |
- /|\\  |
- / \\  |
-      |
-=========`
-];
-HANGMANPICS.forEach((elem) => console.log(elem));
-export {};
+import fs from "fs";
+import { ask } from "../tasker/lib/input.js";
+const words = JSON.parse(fs.readFileSync("./src/hangman/words.json", "utf-8"));
+const randomIndex = Math.floor(Math.random() * words.length);
+const original = words[randomIndex].word;
+console.log(original);
+// const charsToReplace = ["a", "n"];
+// const regex = new RegExp(`[${charsToReplace.join("")}]`, "g");
+// const result = original.replace(regex, " _ ");
+// console.log(result); 
 //# sourceMappingURL=index.js.map
--- src/hangman/index.ts
+++ src/hangman/index.ts
@@ -1,60 +1,22 @@
+import fs from "fs"
+import { ask } from "../tasker/lib/input.js";
+
+type word ={ word: string; category: string }
 
 
+const words:word[] = JSON.parse(
+  fs.readFileSync("./src/hangman/words.json", "utf-8")
+);
 
-const HANGMANPICS:string[]= 
-[
+const randomIndex:number = Math.floor(Math.random()*words.length)
 
-`  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========`,
+
  
-` +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========`,
-`  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========`, 
-`  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========`, 
-`  +---+
-  |   |
-  O   |
- /|\\  |
-      |
-      |
-=========`, 
-`  +---+
-  |   |
-  O   |
- /|\\  |
- /    |
-      |
-=========`, 
-`  +---+
-  |   |
-  O   |
- /|\\  |
- / \\  |
-      |
-=========`
-]
+const original:string =words[randomIndex]!.word ;
+console.log(original)
+// const charsToReplace = ["a", "n"];
 
+// const regex = new RegExp(`[${charsToReplace.join("")}]`, "g");
+// const result = original.replace(regex, " _ ");
 
-HANGMANPICS.forEach((elem)=>console.log(elem))
+// console.log(result); 
